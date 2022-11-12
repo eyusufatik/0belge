fd55166b5f259ae11ee85f0b985e1e2593ea1d35
Ekrem BAL <ekrem@pragmacraft.com>
2022-11-12 16:41:06 +0300

    Not working circuit

--- backend/cli.js
+++ backend/cli.js
@@ -22,7 +22,6 @@ const { GasPriceOracle } = require('gas-price-oracle');
 const SocksProxyAgent = require('socks-proxy-agent');
 const is_ip_private = require('private-ip');
 
-const N = 8;
 
 /** Whether we are in a browser or node.js */
 const inBrowser = typeof window !== 'undefined';
@@ -59,12 +58,41 @@ const string2chunks = (str) => {
 // }
 
 const generateProof = async (str) => {
+  const N = 3;
   const chunks = string2chunks(str);
   // if chunks size is less than N then pad with zeros
   const paddedChunks = chunks.length < N ? [...chunks, ...Array(N - chunks.length).fill(Buffer.alloc(31))] : chunks;
   console.log(paddedChunks);
   const chunksInput = paddedChunks.map((c) => BigInt(bigInt.leBuff2int(c)));
-  const hash = pedersenHash(Buffer.concat(paddedChunks));
+  // // split paddedChunks into 8 chunks
+  // const chunks8 = [];
+  // for(let i = 0; i < paddedChunks.length; i += 2) {
+  //   const hashvalue = pedersenHash(Buffer.concat(paddedChunks.slice(i, i + 2)));
+  //   console.log("hashvalue=>", hashvalue);
+  //   // convert hashvalue to Buffer from bigint
+  //   const hashvalueBuffer = bigInt.beInt2Buff(hashvalue, 32);
+  //   chunks8.push(hashvalueBuffer);
+  // }
+
+
+
+  // convert bigint to buffer
+  // const chunks8Buffer = chunks8.map((c) => bigInt.leInt2Buff(c, 32));
+  const hash0 = pedersenHash(bigInt.leInt2Buff(BigInt(123), 31));
+  const hash1 = pedersenHash(Buffer.concat(paddedChunks));
+  const hash2 = pedersenHash(bigInt.leInt2Buff(BigInt(123), 31));
+  console.log("hash0=>", hash0);
+  console.log("hash1=>", hash1);
+  console.log("hash2=>", hash2);
+
+  const hash0Buffer = bigInt.leInt2Buff(hash0, 32);
+  // get first 31 bits of hash0
+  const hash1Buffer = bigInt.leInt2Buff(hash1, 32);
+  const hash2Buffer = bigInt.leInt2Buff(hash2, 32);
+  // concatanate all the hashes
+  const hashes = Buffer.concat([hash0Buffer.slice(0, 31), hash1Buffer.slice(0, 31), hash2Buffer.slice(0, 31)]);
+  const hash = pedersenHash(hashes);
+
   // array of 256 integers
   const input = {
     // public input
@@ -72,6 +100,8 @@ const generateProof = async (str) => {
 
     // private input 
     chunks: chunksInput,
+    left: bigInt(hash0),
+    right: bigInt(hash2)
   }
   console.log("hash=>", input.belgeHash);
   console.log("chunks=>", input.chunks);
